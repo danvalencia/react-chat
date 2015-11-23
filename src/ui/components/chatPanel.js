c99198faf206bb3f3d92e075ae1e3af5d696ac8a
Daniel Valencia Backhoff <danvalencia@gmail.com>
2015-11-23 08:35:20 -0600

    Clearing text field on focus if text is the placeholder

--- src/ui/components/chatPanel.js
+++ src/ui/components/chatPanel.js
@@ -19,9 +19,11 @@ class ChatPanel extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {value: 'Enter your message here...',
+    this.defaultAreaMessage = 'Enter your message here...';
+    this.state = {value: this.defaultAreaMessage,
                   messages: []};
   }
+
 
   handleTextAreaChange() {
     this.setState({value: event.target.value});
@@ -29,9 +31,17 @@ class ChatPanel extends Component {
 
   handleBroadcastMessage(message) {
     console.log(`Message is ${message}`);
+
     // this.setState((previousState, currentProps) => {
     //   previousState.messages.push()
     // })
+  }
+
+  handleFocus() {
+    const textAreaValue = this.refs.messageInput.value;
+    if(textAreaValue.trim() === this.defaultAreaMessage) {
+      this.setState({value: ""});
+    }
   }
 
   handleMessageSend(webSocket) {
@@ -56,7 +66,8 @@ class ChatPanel extends Component {
           <textarea ref="messageInput"
                     style={styles.textArea} name="message-input-txt-area" rows="2"
                     value={value}
-                    onChange={this.handleTextAreaChange.bind(this)}></textarea>
+                    onChange={this.handleTextAreaChange.bind(this)}
+                    onFocus={this.handleFocus.bind(this)}></textarea>
           <button style={styles.button} onClick={this.handleMessageSend.bind(this, webSocket)}>Send</button>
         </div>
       </div>
